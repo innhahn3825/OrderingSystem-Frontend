b9514e2ea14a64264bf40a1742a6a0b107faae89
Innhahn <jaytan3825@gmail.com>
2022-09-09 23:32:09 +0800

    Merging changes PaymentModal

---
+++ renderer/src/components/PaymentPage/PaymentModal/PaymentModal.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './PaymentModal.module.scss';
+
+const PaymentModal = () => {
+  return (
+    <div className={styles['PaymentModal']}>
+
+      
+
+    </div>
+  )
+}
+
+export default PaymentModal
+
